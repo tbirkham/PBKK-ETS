92e95d6af2bf25f402e1f0f51f6a61404af475e2
Tubagus Irkham <47815216+tbirkham@users.noreply.github.com>
2020-03-30 00:50:30 +0700

    Add files via upload

---
+++ application/views/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+<center>
+  <strong>Copyright &copy; 2020 <a>Tubagus Irkham with AdminLTE</a>.</strong>
+</center>
+<div class="float-right d-none d-sm-inline-block">
+</div>
+</footer>
